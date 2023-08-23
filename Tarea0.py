243a2d9ad3cb1837a1c2d87ede43f5b6f2c75c78
unknown <francisco.gutial@gmail.com>
2023-08-22 22:22:09 -0400

    Codigo modificado para que dibuje un hexagono

--- Tarea0.py
+++ Tarea0.py
@@ -2,6 +2,8 @@ import pyglet
 from OpenGL import GL
 import numpy as np
 
+WIDTH = 480
+HEIGHT = 640
 class Controller(pyglet.window.Window):
     def __init__(self, title,*args,**kargs):
         super().__init__(*args,**kargs)
@@ -9,9 +11,8 @@ class Controller(pyglet.window.Window):
         self.set_caption(title)
         
 
-
 if __name__=="__main__":
-    controller = Controller("Tarea 0", resizable=True)
+    controller = Controller("Tarea 0", width=WIDTH, height=HEIGHT, resizable=True)
     
     vertex_source_code = """
         #version 330
@@ -51,31 +52,45 @@ frag_shader =pyglet.graphics.shader.Shader(fragment_source_code,"fragment")
 pipeline = pyglet.graphics.shader.ShaderProgram(vert_shader, frag_shader)
 
 positions = np.array([
-        -0.5, -0.5,
-         0.5, -0.5, 
-         0.0,  0.5
+        -0.2, -0.2,
+         0.2, -0.2, 
+         0.4,  0.0,
+         0.2,  0.2,
+        -0.2,  0.2,
+        -0.4,  0.0
+
     ], dtype=np.float32)
 
 colors = np.array([
         1, 0, 0,
         0, 1, 0,
-        0, 0, 1
+        0, 0, 1,
+        1, 1, 1,
+        0, 1, 1,
+        1, 1, 0
     ], dtype=np.float32)
 
 intensities = np.array([
-        1, 0.5, 0
+        1, 1, 1, 1, 1, 1
     ], dtype=np.float32)
 
-gpu_triangle = pipeline.vertex_list(3, GL.GL_TRIANGLES)
-gpu_triangle.position = positions
-gpu_triangle.color = colors
-gpu_triangle.intensity = intensities
+indices = np.array([
+    0, 1, 2,
+    0, 2, 5,
+    5, 2, 3,
+    5, 3, 4
+    ], dtype=np.uint32)
+
+gpu_hex = pipeline.vertex_list_indexed(6, GL.GL_TRIANGLES, indices)
+gpu_hex.position = positions
+gpu_hex.color = colors
+gpu_hex.intensity = intensities
 
 @controller.event
 def on_draw():
     GL.glClearColor(0,0,0,1.0)
     controller.clear()
     pipeline.use()
-    gpu_triangle.draw(GL.GL_TRIANGLES)  
-    
+    gpu_hex.draw(GL.GL_TRIANGLES)  
+
 pyglet.app.run()
