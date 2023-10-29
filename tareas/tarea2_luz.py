8ebbb0ec92983918d4c5ad9965d985ddeeeda78c
fgutierrezalbornoz <francisco.gutial@gmail.com>
2023-10-28 23:31:12 -0300

    Agrega nueva funcion para mostrar distintos autos

--- tareas/tarea2_luz.py
+++ tareas/tarea2_luz.py
@@ -75,6 +75,39 @@ class Car():
                 )
         
 
+class Car2():
+    def __init__(self, chassis_mesh, wheel_mesh, platform_mesh, graph, 
+                 material, i=0, chassis_pos=[0,0,0], wheel_pos=[0,0,0], pos=[0,0,0]):
+
+        chassis_material, wheel_material, platform_material=material
+        graph.add_node("car_system_"+str(i),position=pos)
+        graph.add_node("platform_"+str(i), attach_to="car_system_"+str(i),mesh=platform_mesh, 
+                       color=[0.5, 0.5, 0.5], pipeline=color_mesh_lit_pipeline, material=platform_material, 
+                       scale=[2,2,2])
+        graph.add_node("car_"+str(i), attach_to="car_system_"+str(i),position=[0,0.24,0])
+        # graph.add_node("car_"+str(i), attach_to="car_system_"+str(i),position=[0.2,0.31,0])
+        graph.add_node("chassis_"+str(i), attach_to="car_"+str(i),mesh=chassis_mesh,
+                       pipeline=color_mesh_lit_pipeline, scale=[1,1,1],material=chassis_material, 
+                       position = chassis_pos)
+        graph.add_node("wheel_"+str(i), attach_to="car_"+str(i), mesh=wheel_mesh,
+                       pipeline=color_mesh_lit_pipeline, scale=[0.7,0.7,0.7], material=wheel_material, position = wheel_pos)
+
+        
+        graph.add_node("spotlight_"+str(i),
+                       attach_to="platform_"+str(i),
+                   pipeline=color_mesh_lit_pipeline,
+                   position=[0, 0.8, 0],
+                   rotation=[-np.pi/2, 0, 0],
+                   light=SpotLight(
+                          diffuse = [1, 1, 1],
+                          specular = [1, 1, 1],
+                          ambient = [0.15, 0.15, 0.15],
+                          cutOff = 0.91, # siempre mayor a outerCutOff
+                          outerCutOff = 0.82
+                   )
+                )
+        
+
         
 
     def draw(self):
@@ -113,6 +146,15 @@ if __name__ == "__main__":
     chassis_mesh = mesh_from_file("testeo/wacky_races/auto_completo.stl")[0]["mesh"]
     wheel_mesh = mesh_from_file("testeo/wacky_races/right_front_wheel.stl")[0]["mesh"]
     platform_mesh = mesh_from_file("testeo/wacky_races/circular_platform.stl")[0]["mesh"]
+
+    chassis1_mesh = mesh_from_file("testeo/wacky_races/mean_machine_chassis.stl")[0]["mesh"]
+    wheel1_mesh = mesh_from_file("testeo/wacky_races/mean_machine_wheels.stl")[0]["mesh"]
+
+    chassis2_mesh = mesh_from_file("testeo/wacky_races/army_surplus_special_chassis.stl")[0]["mesh"]
+    wheel2_mesh = mesh_from_file("testeo/wacky_races/army_surplus_special_wheels.stl")[0]["mesh"]
+
+    chassis3_mesh = mesh_from_file("testeo/wacky_races/turbo_terrific_chassis.stl")[0]["mesh"]
+    wheel3_mesh = mesh_from_file("testeo/wacky_races/turbo_terrific_wheels.stl")[0]["mesh"]
 
     graph = SceneGraph(controller)
 
@@ -154,10 +196,16 @@ if __name__ == "__main__":
         specular =	[0.633, 0.727811, 0.633],
         ambient	= [0.0215, 0.1745, 0.0215],
         shininess = sh *0.6)
+
+
+
+    # Car(chassis_mesh, wheel_mesh, platform_mesh, graph, [material, rubber, silver], pos=[0,0,0])
     
-    Car(chassis_mesh, wheel_mesh, platform_mesh, graph, [material, rubber, silver], pos=[0,0,0])
-    Car(chassis_mesh, wheel_mesh, platform_mesh, graph, [emerald, rubber,  silver], i=1,pos=[-5,0,0])
-    Car(chassis_mesh, wheel_mesh, platform_mesh, graph, [gold, rubber, silver], i=2,pos=[5,0,0])
+    # Car(chassis_mesh, wheel_mesh, platform_mesh, graph, [emerald, rubber,  silver], i=1,pos=[-5,0,0])
+    # Car(chassis_mesh, wheel_mesh, platform_mesh, graph, [gold, rubber, silver], i=2,pos=[5,0,0])
+    Car2(chassis1_mesh, wheel1_mesh, platform_mesh, graph, [material, rubber, silver], i=0,pos=[0,0,0], chassis_pos=[0.5,0.1,0],wheel_pos=[0,-0.07,0])
+    Car2(chassis2_mesh, wheel2_mesh, platform_mesh, graph, [emerald, rubber,  silver], i=1,pos=[-5,0,0], chassis_pos=[-0.125,0.355,0])
+    Car2(chassis3_mesh, wheel3_mesh, platform_mesh, graph, [gold, rubber, silver], i=2,pos=[5,0,0], chassis_pos=[-0.025,-0.045,0],wheel_pos=[-0.4,0,0])
 
 
     graph.add_node("light",
@@ -191,19 +239,19 @@ if __name__ == "__main__":
     # shininess: Exponente especular del material
 
     posiciones = [[-5,1.5,2], [0,1.5,2], [5,1.5,2]]
-    k=0
+    k=2
     def update(dt):
         global k
         global posiciones
         controller.program_state["total_time"] += dt
         camera = controller.program_state["camera"]
 
-        graph["car_1"]["rotation"][1] += 2*dt
-        graph["platform_1"]["rotation"][1] += 2*dt
-        graph["car_0"]["rotation"][1] += 2*dt
-        graph["platform_0"]["rotation"][1] += 2*dt
-        graph["car_2"]["rotation"][1] += 2*dt
-        graph["platform_2"]["rotation"][1] += 2*dt
+        # graph["car_1"]["rotation"][1] += 2*dt
+        # graph["platform_1"]["rotation"][1] += 2*dt
+        # graph["car_0"]["rotation"][1] += 2*dt
+        # graph["platform_0"]["rotation"][1] += 2*dt
+        # graph["car_2"]["rotation"][1] += 2*dt
+        # graph["platform_2"]["rotation"][1] += 2*dt
 
         
         if controller.is_key_pressed(pyglet.window.key.A):
